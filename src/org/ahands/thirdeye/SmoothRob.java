2981379d2437fb13ce90432cf638909d3f25ae56
Ian Page Hands <iphands@gmail.com>
2010-11-01 21:33:37 -0400

    added a class that smooths the mouse movements

---
+++ src/org/ahands/thirdeye/SmoothRob.java
@@ -0,0 +1,77 @@
+package org.ahands.thirdeye;
+
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+
+public class SmoothRob {
+
+	final Robot rob;
+	final Point origin;
+	final int threshold;
+
+	public SmoothRob(final Point origin, final int threshold) throws AWTException {
+		this.rob = new Robot();
+		this.origin = origin;
+		this.threshold = threshold;
+	}
+
+	private void sMoveX(Point current, int distance) {
+		for (int i = 0, j = Math.abs(distance); i < j; i++) {
+			if (distance < 0) {
+				current.x++;
+			} else {
+				current.x--;
+			}
+			rob.mouseMove(current.x, current.y);
+		}
+	}
+
+	private void sMoveY(Point current, int distance) {
+		for (int i = 0, j = Math.abs(distance); i < j; i++) {
+			if (distance > 0) {
+				current.y++;
+			} else {
+				current.y--;
+			}
+			rob.mouseMove(current.x, current.y);
+		}
+	}
+
+	public void smoothMouseMove(Point dotLocation) {
+		Point current = MouseInfo.getPointerInfo().getLocation();
+
+		if (dotLocation.x != (origin.x + threshold)) {
+			int distance = dotLocation.x - origin.x;
+			this.sMoveX(current, distance);
+		}
+
+		current = MouseInfo.getPointerInfo().getLocation();
+		if (dotLocation.y != (origin.y + threshold)) {
+			int distance = dotLocation.y - origin.y;
+			this.sMoveY(current, distance);
+		}
+	}
+
+	public void moveMouse(Point dotLocation) {
+		Point current = MouseInfo.getPointerInfo().getLocation();
+
+		if (dotLocation.x > (origin.x + threshold)) {
+			int rate = dotLocation.x - origin.x;
+			rob.mouseMove(current.x - (rate), current.y);
+		} else if (dotLocation.x < (origin.x - threshold)) {
+			int rate = origin.x - dotLocation.x;
+			rob.mouseMove(current.x + (rate), current.y);
+		}
+
+		current = MouseInfo.getPointerInfo().getLocation();
+		if (dotLocation.y > (origin.y + threshold)) {
+			int rate = dotLocation.y - origin.y;
+			rob.mouseMove(current.x, (current.y + rate));
+		} else if (dotLocation.y < (origin.y - threshold)) {
+			int rate = origin.y - dotLocation.y;
+			rob.mouseMove(current.x, (current.y - rate));
+		}
+	}
+}
--- src/org/ahands/thirdeye/ThirdEye.java
+++ src/org/ahands/thirdeye/ThirdEye.java
@@ -3,10 +3,7 @@ package org.ahands.thirdeye;
 import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.MouseInfo;
 import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.Robot;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
@@ -17,7 +14,6 @@ public class ThirdEye {
 	public static void main(String[] args) throws AWTException {
 		final Cam cam = new Cam("/dev/video0");
 		final Dot dot = new Dot();
-		final Robot rob = new Robot();
 		final JFrame frame = new JFrame("Image");
 		BufferedImage bImg;
 		bImg = cam.getImg();
@@ -30,14 +26,14 @@ public class ThirdEye {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		boolean isInBox = true;
 		Point dotLocation = new Point(0, 0);
 
 		final Graphics2D g2d = (Graphics2D) frame.getRootPane().getGraphics();
 		// final Rectangle container = (dot.getSimpleContainer());
 		final Ellipse2D container = (dot.getRoundContainer());
 		final Point origin = dot.getAverage();
-		int threshold = 5;
+		final int threshold = 5;
+		final SmoothRob smoothRob = new SmoothRob(origin, threshold);
 		while (true) {
 
 			// bImg = cam.getFlippedImg();
@@ -46,38 +42,22 @@ public class ThirdEye {
 			dot.findDot();
 			dotLocation = dot.getAverage();
 
-			// bImg.setRGB(dotLocation.x, dotLocation.y, 0xffff0000);
 			g2d.drawImage(bImg, null, 0, 0);
 			g2d.drawImage(dot.getDotImg(), null, W, 0);
 
 			// Circle
 			final Shape circle = new Ellipse2D.Float(dotLocation.x - 2, dotLocation.y - 2, 8, 8);
-			g2d.draw(circle);
 			g2d.setPaint(Color.blue);
+			g2d.draw(circle);
 			g2d.fill(circle);
 
 			// Box
+			g2d.setPaint(Color.green);
 			g2d.draw(container);
-			g2d.setPaint(Color.yellow);
 
 			if (dotLocation != origin) {
-				Point current = MouseInfo.getPointerInfo().getLocation();
-				if (dotLocation.x > (origin.x + threshold)) {
-					int rate = dotLocation.x - origin.x;
-					rob.mouseMove(current.x - (rate), current.y);
-				} else if (dotLocation.x < (origin.x - threshold)) {
-					int rate = origin.x - dotLocation.x;
-					rob.mouseMove(current.x + (rate), current.y);
-				}
-
-				current = MouseInfo.getPointerInfo().getLocation();
-				if (dotLocation.y > (origin.y + threshold)) {
-					int rate = dotLocation.y - origin.y;
-					rob.mouseMove(current.x, (current.y + rate));
-				} else if (dotLocation.y < (origin.y - threshold)) {
-					int rate = origin.y - dotLocation.y;
-					rob.mouseMove(current.x, (current.y - rate));
-				}
+				smoothRob.moveMouse(dotLocation);
+				// smoothRob.smoothMouseMove(dotLocation);
 			}
 		}
 	}
